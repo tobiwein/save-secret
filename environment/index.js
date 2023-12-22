1564f67e05fd19833b2a3b3ddf8b8fdb3c8dfd55
TobiasWeinlich <tobias.weinlich@gmx.de>
2023-12-22 20:37:11 +0100

    add inputs

--- environment/index.js
+++ environment/index.js
@@ -3,19 +3,26 @@ import github from '@actions/github';
 import { Octokit } from 'octokit';
 
 async function run() {
-    const api = core.getInput('githubApi');
     const token = core.getInput('token');
-    const repo = core.getInput('repositoryId');
     const env = core.getInput('environmentName');
-    const toEnc = core.getInput("valueToEncrypt");
+    const toEnc = core.getInput("secret");
     const name = core.getInput("secretName");
+
+    let api = core.getInput('githubApi');
+    if (api === '') {
+        api = github.context.apiUrl;
+    }
+    let repo = core.getInput('repositoryId');
+    if (repo === '') {
+        repo = github.context.repositoryId;
+    }
 
     const publicKeyUrl = "/repositories/" + repo + "/environments/" + env + "/secrets/public-key";
     const publicKey = getPublicKey(api, token, publicKeyUrl);
     const key = publicKey.key;
     const keyId = publicKey.KeyId;
 
-    const encryptedValue = encryptValue(valueToEncrypt, key);
+    const encryptedValue = encryptValue(secret, key);
     const environmentSecretUrl = "repositories/" + repo + "/environments/" + env + "/secrets/" name;
     saveSecret(api, token, environmentSecretUrl, encryptedValue, keyId);
 }

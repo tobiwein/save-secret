d0d3e8c31cf67365506316e0e771c7d5f9c70d45
Tobias <51930902+tobiwein@users.noreply.github.com>
2023-12-24 21:22:28 +0000

    add mask to sensitive values

--- environment/index.js
+++ environment/index.js
@@ -1,14 +1,13 @@
 import core from '@actions/core';
 import github from '@actions/github';
-import { Octokit } from 'octokit';
 import { getPublicKey, encryptValue, saveSecret } from '../common/main.js';
 import { log, debug, logJson, debugJson } from '../common/log.js';
 
 async function run() {
-    const token = core.getInput('token');
+    const token = core.setSecret(core.getInput('token'));
     const repo = core.getInput('repositoryId');
     const env = core.getInput('environmentName');
-    const secret = core.getInput("secret");
+    const secret = core.setSecret(core.getInput("secret"));
     const name = core.getInput("secretName");
 
     let api = core.getInput('githubApi');
@@ -16,25 +15,13 @@ async function run() {
         api = github.context.apiUrl;
     }
 
-    log(token)
-    log(env)
-    log(secret)
-    log(name)
-    log(api)
-    log(repo)
+    const publicKeyUrl = `/repositories/${repo}/environments/${env}/secrets/public-key`;
+    const publicKey = core.setSecret(await getPublicKey(api, token, publicKeyUrl));
+    const key = core.setSecret(publicKey.key);
+    const keyId = core.setSecret(publicKey.keyId);
 
-    const publicKeyUrl = "/repositories/" + repo + "/environments/" + env + "/secrets/public-key";
-    log(publicKeyUrl)
-    const publicKey = await getPublicKey(api, token, publicKeyUrl);
-    log(publicKey)
-    const key = publicKey.key;
-    log(key)
-    const keyId = publicKey.keyId;
-    log(keyId)
-
-    const encryptedValue = await encryptValue(secret, key);
-    log("enc: " + encryptedValue);
-    const environmentSecretUrl = "/repositories/" + repo + "/environments/" + env + "/secrets/" + name;
+    const encryptedValue = core.setSecret(await encryptValue(secret, key));
+    const environmentSecretUrl = `/repositories/${repo}/environments/${env}/secrets/${name}`;
     saveSecret(api, token, environmentSecretUrl, encryptedValue, keyId);
 }
 
--- organisation/index.js
+++ organisation/index.js
@@ -1,13 +1,12 @@
 import core from '@actions/core';
 import github from '@actions/github';
-import { Octokit } from 'octokit';
 import { getPublicKey, encryptValue, saveSecret } from '../common/main.js';
 import { log, debug, logJson, debugJson } from '../common/log.js';
 
 async function run() {
-    const token = core.getInput('token');
+    const token = core.setSecret(core.getInput('token'));
     const org = core.getInput('organisation');
-    const secret = core.getInput("secret");
+    const secret = core.setSecret(core.getInput("secret"));
     const name = core.getInput("secretName");
 
     let api = core.getInput('githubApi');
@@ -15,24 +14,13 @@ async function run() {
         api = github.context.apiUrl;
     }
 
-    log(token)
-    log(secret)
-    log(api)
-    log(name)
-    log(org)
+    const publicKeyUrl = `/orgs/${org}/actions/secrets/public-key`;
+    const publicKey = core.setSecret(await getPublicKey(api, token, publicKeyUrl));
+    const key = core.setSecret(publicKey.key);
+    const keyId = core.setSecret(publicKey.keyId);
 
-    const publicKeyUrl = "/orgs/" + org + "/actions/secrets/public-key";
-    log(publicKeyUrl)
-    const publicKey = await getPublicKey(api, token, publicKeyUrl);
-    log(publicKey)
-    const key = publicKey.key;
-    log(key)
-    const keyId = publicKey.keyId;
-    log(keyId)
-
-    const encryptedValue = await encryptValue(secret, key);
-    log("enc: " + encryptedValue);
-    const orgSecretUrl = "/orgs/" + org + "/actions/secrets/" + name;
+    const encryptedValue = core.setSecret(await encryptValue(secret, key));
+    const orgSecretUrl = `/orgs/${org}/actions/secrets/${name}`;
     saveSecret(api, token, orgSecretUrl, encryptedValue, keyId);
 }
 
